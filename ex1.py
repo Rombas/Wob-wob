ba0071a77f6d29e86fc9b14cea85259f56ac840d
rombas <RomanBash@gmail.com>
2012-09-20 21:23:48 +0400

    g

--- ex1.py
+++ ex1.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
-year = raw_input("Enter current year: ")
-year = int(year)
-if year%400 == 0:
-    print "It's leap year!"
-elif year%100 == 0:
-    print "It's not leap year!"
-elif year%4 == 0:
-    print "It's leap year!"
+try:
+    year = float(raw_input("\nEnter current year: "))
+except(ValueError):
+    print "That was not a number!" 
 else:
-    print "It's not leap year!"
+    if year%400 == 0:
+        print "It's leap year!"
+    elif year%100 == 0:
+        print "It's not leap year!"
+    elif year%4 == 0:
+        print "It's leap year!"
+    else:
+        print "It's not leap year!"
 
-raw_input ("Press enter for closing.")
+raw_input ("Press enter for exit.")
+        
